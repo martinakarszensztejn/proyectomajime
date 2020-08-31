0f013aa89d0dc32b95373a7fdbbfbfe605520c62
Martina <martinakarszensztejn@tutanota.com>
2020-08-30 21:19:02 -0300

    MER Mejorado. Pequeños cambios en programa. Atributos y RNE

--- Majime VB 2808/MainGerente.Designer.vb
+++ Majime VB 2808/MainGerente.Designer.vb
@@ -31,6 +31,7 @@ Partial Class MainGerente
         Me.Button7 = New System.Windows.Forms.Button()
         Me.Button8 = New System.Windows.Forms.Button()
         Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
@@ -114,11 +115,21 @@ Partial Class MainGerente
         Me.Button9.Text = "Salir de la Sesión"
         Me.Button9.UseVisualStyleBackColor = True
         '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(48, 147)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(141, 23)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Confirmar Check In"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
         'MainGerente
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button10)
         Me.Controls.Add(Me.Button9)
         Me.Controls.Add(Me.Button7)
         Me.Controls.Add(Me.Button8)
@@ -143,4 +154,5 @@ Partial Class MainGerente
     Friend WithEvents Button7 As Button
     Friend WithEvents Button8 As Button
     Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
 End Class
