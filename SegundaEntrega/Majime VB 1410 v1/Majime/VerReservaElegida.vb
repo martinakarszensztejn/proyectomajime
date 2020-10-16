ccef6700e3b7bdc18500a5c55163064a3bfb81d3
Martina <martinakarszensztejn@tutanota.com>
2020-10-15 21:25:55 -0300

    Fixeos en la UTU

---
+++ SegundaEntrega/Majime VB 1410 v1/Majime/VerReservaElegida.vb
@@ -0,0 +1,11 @@
+﻿Public Class VerReservaElegida
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Guau.rangoUsuario = Guau.rangos(0) Then
+            Guau.AbrirFormEnPanel(New MainGerente)
+        ElseIf Guau.rangoUsuario = Guau.rangos(2) Then
+            Guau.AbrirFormEnPanel(New MainRecepcionistaGaraje)
+        ElseIf Guau.rangoUsuario = Guau.rangos(1) Then
+            Guau.AbrirFormEnPanel(New MainOficinista)
+        End If
+    End Sub
+End Class
