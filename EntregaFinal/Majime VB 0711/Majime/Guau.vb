e39c4d621ca6cfcf0e054e6d0f61496d6b9ee0be
Martina <martinakarszensztejn@tutanota.com>
2020-11-07 20:41:23 -0300

    Altas de mascotas sin testear, mas validaciones y calculo de precio q permite mas de una mascota

---
+++ EntregaFinal/Majime VB 0711/Majime/Guau.vb
@@ -0,0 +1,53 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Guau
+
+    Public rangos() As String = {"Gerente", "Oficinista", "Recepcionista de Garaje"}
+    Public rangoUsuario As String
+    Public userUsuario As String
+    Public clavesDeRangoPrivadas() As String = {"c4zzu", "tr1", "n1ck1"}
+
+    Public connnn As New MySqlConnectionStringBuilder
+    Public conk As New MySqlConnection
+
+
+    Public Sub AbrirFormEnPanel(ByVal FormHijo As Object)
+        If Me.Panel1.Controls.Count > 0 Then
+            Me.Panel1.Controls.RemoveAt(0)
+        End If
+        Dim fh As Form = TryCast(FormHijo, Form)
+        fh.TopLevel = False
+        fh.FormBorderStyle = FormBorderStyle.None
+        fh.Dock = DockStyle.Fill
+        fh.MdiParent = Me.MdiParent
+        Me.Panel1.Controls.Add(fh)
+        Me.Panel1.Tag = fh
+        fh.Show()
+
+    End Sub
+
+
+
+    Private Sub Guau_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AbrirFormEnPanel(New Form1)
+
+        Try
+            Dim connnn As New MySqlConnectionStringBuilder()
+            connnn.UserID = "root"
+            connnn.Server = "localhost"
+            connnn.Database = "mkarszensztejn"
+            connnn.Password = "password"
+            Dim connk As New MySqlConnection(connnn.ToString)
+            conk = connk
+
+            If Me.conk.State = ConnectionState.Open Then
+                Me.conk.Close()
+                Me.conk.Open()
+            Else
+                Me.conk.Open()
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
---
+++ EntregaFinal/Majime VB 0711/Majime/Horario.vb
@@ -0,0 +1,11 @@
+﻿Public Class Horario
+    Public num_periodo As Int32
+    Public hora_inicio As DateTime
+    Public hora_fin As DateTime
+
+    Public Sub New(num_periodo As Integer, hora_inicio As Date, hora_fin As Date)
+        Me.num_periodo = num_periodo
+        Me.hora_inicio = hora_inicio
+        Me.hora_fin = hora_fin
+    End Sub
+End Class
